a3f14bcc1709b5d2ffc6d24692e79d8a7d77f5a7
ahmet <adsayahmet94@hotmail.com>
2023-04-24 17:20:44 +0300

    Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const form = document.getElementById('create-task-form')
+const inputUser = document.getElementById('new-task-description')
+const deleteButton = document.getElementById('delete-button')
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    inputRendered(inputUser.value)
+    inputUser.value = ''
+  })
 });
+
+deleteButton.addEventListener('click', function(e) {
+   const taskList = document.querySelector('#tasks')
+   taskList.innerHTML = ''
+})
+
+
+function inputRendered(input) {
+  let list = document.createElement('li')
+  list.textContent = input
+  document.querySelector('#tasks').appendChild(list)
+
+}
